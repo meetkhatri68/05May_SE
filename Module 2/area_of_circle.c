3622339dde43ef47dab97e12b1082f1f554ca722
meetkhatri68 <machchhars@gmail.com>
2023-07-19 18:44:23 +0530

    19/07/2023

--- Module 2/area_of_circle.c
+++ Module 2/area_of_circle.c
@@ -1,15 +1,15 @@
-//wap to find area of circle(formula of circle c=pi*r*r)
-#include<stdio.h>
-int  main()
+// wap to find area of circle(formula of circle c=pi*r*r)
+#include <stdio.h>
+int main()
 {
-    float pi=3.14;
-    float r,c;
+    float pi = 3.14;
+    float r, c;
     printf("enter the value of r : ");
-    scanf("%f",&r);
-    
-    c=pi*r*r;
+    scanf("%f", &r);
 
-    printf("area of circle is %f",c);
+    c = pi * r * r;
+
+    printf("area of circle is %f", c);
 
     return 0;
-}   
+}
